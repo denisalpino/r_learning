c7dda66467c2a298d0640a5df2c2963974b41592
Denis Alpino <denis.tomin.alpino@gmail.com>
2022-10-27 04:10:43 +0300

    Update meeting.R

--- meeting.R
+++ meeting.R
@@ -1,5 +1,12 @@
-print("Hello world")
 
-a <- "hello world"
-print(a)
-
+# rm - функция, которая удаляет переменные
+# function_name <- function(x, y, ...) {
+#   name_of_object <- some operations
+#   return(name_of_object)
+# } - данная функция обладает сторонним эффектом, так как принимает значение
+# из-вне и преобразовывает его
+# function_name <- function() {
+#   name_of_object <- some operations
+#   return(name_of_object)
+# } - данная функция называется обкновенной, так как является постоянной и
+# и всегда возвращает одинаковое значение
